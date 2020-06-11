a17f319ff1eb37da0e2bdd53e1e83861eac97840
Sha-jay Perry <shajayjanae@gmail.com>
2020-06-11 04:18:02 +0000

    Automatically backed up by Learn

--- lib/song.rb
+++ lib/song.rb
@@ -21,3 +21,18 @@ class Song
     @@all
   end
 end
+
+
+
+ def self.new_by_filename(file)
+    song_name = file.split(" - ")[1]
+    artist = file.split(" - ")[0]
+    song = self.new(song_name)
+    song.artist_name = artist
+    song
+  end
+  
+  def artist_name=(name)
+    self.artist = Artist.find_or_create_by_name(name)
+    self.artist.add_song(self)
+  end
